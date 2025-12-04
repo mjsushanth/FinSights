b1a7ddf3581fe711e960e93c455564f2f601bf2d
karthiks233 <karthiks233@gmail.com>
2025-12-04 00:03:58 -0500

    Adding Docker fix

--- DataPipeline/src_metrics/trash.py
+++ DataPipeline/src_metrics/trash.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-df = pd.read_parquet('~/Downloads/analytical_layer_metrics_final_test.parquet')
+df = pd.read_parquet('~/Downloads/analytical_layer_metrics_final.parquet')
 
 
 print(df.isnull().sum())
+
+print(df.shape)
