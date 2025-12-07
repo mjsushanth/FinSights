7038032a40524f201d717cbbbc45b1e23c7c893b
Sridipta Roy <roysr.ds@gmail.com>
2025-12-07 04:30:48 -0500

    Refactor AIRFLOW_HOME and directory setup logic

--- DataPipeline/src/__init__.py
+++ DataPipeline/src/__init__.py
@@ -1,22 +1,19 @@
 from pathlib import Path
 import os
 
-AIRFLOW_HOME = Path(os.environ.get("AIRFLOW_HOME", "/opt/airflow"))
-# Directories relative to project root
-DATASET_DIR = AIRFLOW_HOME / "datasets"
-LOGGING_DIR = AIRFLOW_HOME / "logs"
-# LOGGING_DIR.mkdir(parents=True, exist_ok=True)
-
-os.makedirs(LOGGING_DIR, exist_ok=True)
-os.makedirs(DATASET_DIR, exist_ok=True)
-
-#for local run
-# from pathlib import Path
-# import os
-
-# AIRFLOW_HOME = Path(os.environ.get("AIRFLOW_HOME", ""))
+# AIRFLOW_HOME = Path(os.environ.get("AIRFLOW_HOME", "/opt/airflow"))
+# # Directories relative to project root
 # DATASET_DIR = AIRFLOW_HOME / "datasets"
 # LOGGING_DIR = AIRFLOW_HOME / "logs"
+# # LOGGING_DIR.mkdir(parents=True, exist_ok=True)
 
 # os.makedirs(LOGGING_DIR, exist_ok=True)
 # os.makedirs(DATASET_DIR, exist_ok=True)
+
+#for local run
+AIRFLOW_HOME = Path(os.environ.get("AIRFLOW_HOME", ""))
+DATASET_DIR = AIRFLOW_HOME / "datasets"
+LOGGING_DIR = AIRFLOW_HOME / "logs"
+
+os.makedirs(LOGGING_DIR, exist_ok=True)
+os.makedirs(DATASET_DIR, exist_ok=True)
